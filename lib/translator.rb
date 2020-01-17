fd194c26c45ddc228dfcd836715da720ae22a7f6
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-17 07:10:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
-  load_library(file_path)
+  load_library(file_path)[get_emoticon][emoticon]
 end
 
 def get_english_meaning
