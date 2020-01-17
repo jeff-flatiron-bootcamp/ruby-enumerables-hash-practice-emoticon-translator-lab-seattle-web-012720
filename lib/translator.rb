956fc0b5e360f4fbc869585759d486b7ca96b50e
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-17 07:07:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,26 +3,11 @@ require "yaml"
 require "pp"
 def load_library(file_path)
   # code goes here
-  
-  fmoticons = ["☜(⌒▽⌒)☞", "(ΘεΘ;)", "(￣ー￣)", "o_O", "(^_-)"]
-  
-  dummy_hash = {}
-  dummy_hash[fmoticons[0]] = ""
-  puts "Dummy_hash"
-  pp dummy_hash
-  
-  puts "\nLoading File"
   hash = YAML.load_file(file_path)
-  pp hash
-  puts "\n"
-  puts "\n"
-  puts "Running reduce on hash"
   jap_meaning_hash = hash.reduce({}) do |memo, (key,value)| 
       memo[value[1]] = key
       memo
   end
-  puts "Outputting hash after reduce"
-  pp hash
   us_meaning_hash = hash.reduce({}) do |memo, (key,value)| 
       memo[value[0]] = value[1]
       memo
